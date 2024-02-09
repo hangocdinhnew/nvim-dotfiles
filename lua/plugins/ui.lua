69433cdd877fe304acbcd42426a1b536e32ee422
dinhndrwnew <dinhndrwnew@dinhndrwnews-MacBook-Pro.local>
2024-02-09 16:04:36 +0700

    add some devaslife stuff lul

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,21 @@
+return {
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+  -- buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev buffer" },
+    },
+  },
+}
