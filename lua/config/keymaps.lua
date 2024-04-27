7b120c6d60588c5c3a8a71db475a71ca12c8c6b2
hangocdinhnew <dinhndrwnew@gmail.com>
2024-04-27 21:13:47 +0700

    Remap Ctrl + l, and h to using Alt instead.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,13 @@
 
 local discipline = require("things.discipline")
 discipline.cowboy()
+
+local opts = { noremap = true, silent = true }
+
+-- Map Ctrl + h to Alt + h
+vim.api.nvim_set_keymap("n", "<A-h>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("i", "<A-h>", "<C-o><C-w>h", opts)
+
+-- Map Ctrl + l to Alt + l
+vim.api.nvim_set_keymap("n", "<A-l>", "<C-w>l", opts)
+vim.api.nvim_set_keymap("i", "<A-l>", "<C-o><C-w>l", opts)
